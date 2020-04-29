f3334876ad5eaf28daf4843dfdfd423b81148882
Exr0n <spotyie@gmail.com>
2020-04-29 16:24:27 -0700

    trying to parse json..

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+use serde::Deserialize;
+use rocket_contrib::json::Json;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
+
+#[derive(Deserialize)]
+struct Task {
+    description: String,
+    complete: bool
+}
+
+#[post("/", data = "<task>")]
+fn new(task: Json<Task>) {
+    println!("Task {} has status {}", task.description, task.complete);
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
+}
+
